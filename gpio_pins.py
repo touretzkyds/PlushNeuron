b8e8cb1d36885c6ca843d1a2ba39c653315f45bf
Dave Touretzky <dst@cs.cmu.edu>
2025-01-03 19:53:52 -0500

    Reorder GPIO pins

--- gpio_pins.py
+++ gpio_pins.py
@@ -2,36 +2,36 @@ import digitalio
 import board
 
 # Dendrite 1
-DENDRITE_1_ROTARY_PINS = [digitalio.DigitalInOut(board.D4),
-                          digitalio.DigitalInOut(board.D17),
-		          digitalio.DigitalInOut(board.D18),
-                          digitalio.DigitalInOut(board.D27)]
-DENDRITE_1_BUTTON_PIN = digitalio.DigitalInOut(board.D22)
-DENDRITE_1_BARREL_PIN = digitalio.DigitalInOut(board.D23)
+DENDRITE_1_ROTARY_PINS = [digitalio.DigitalInOut(board.D17),
+                          digitalio.DigitalInOut(board.D18),
+		          digitalio.DigitalInOut(board.D27),
+                          digitalio.DigitalInOut(board.D22)]
+DENDRITE_1_BUTTON_PIN = digitalio.DigitalInOut(board.D23)
+DENDRITE_1_BARREL_PIN = digitalio.DigitalInOut(board.D24)
 
 # Dendrite 2
-DENDRITE_2_ROTARY_PINS = [digitalio.DigitalInOut(board.D24),
+DENDRITE_2_ROTARY_PINS = [digitalio.DigitalInOut(board.D9),
                           digitalio.DigitalInOut(board.D25),
-		          digitalio.DigitalInOut(board.D9),
-                          digitalio.DigitalInOut(board.D8)]
-DENDRITE_2_BUTTON_PIN = digitalio.DigitalInOut(board.D7)
-DENDRITE_2_BARREL_PIN = digitalio.DigitalInOut(board.D5)
+		          digitalio.DigitalInOut(board.D8),
+                          digitalio.DigitalInOut(board.D7)]
+DENDRITE_2_BUTTON_PIN = digitalio.DigitalInOut(board.D5)
+DENDRITE_2_BARREL_PIN = digitalio.DigitalInOut(board.D6)
 
 # Dendrite 3
-DENDRITE_3_ROTARY_PINS = [digitalio.DigitalInOut(board.D6),
-                          digitalio.DigitalInOut(board.D12),
-		          digitalio.DigitalInOut(board.D13),
+DENDRITE_3_ROTARY_PINS = [digitalio.DigitalInOut(board.D12),
+                          digitalio.DigitalInOut(board.D13),
+		          digitalio.DigitalInOut(board.D19),
                           digitalio.DigitalInOut(board.D16)]
-DENDRITE_3_BUTTON_PIN = digitalio.DigitalInOut(board.D19)
+DENDRITE_3_BUTTON_PIN = digitalio.DigitalInOut(board.D26)
 DENDRITE_3_BARREL_PIN = digitalio.DigitalInOut(board.D20)
 
 # Threshold
 THRESHOLD_ROTARY_PINS = [digitalio.DigitalInOut(board.D2),
                          digitalio.DigitalInOut(board.D3),
-		         digitalio.DigitalInOut(board.D14),
-                         digitalio.DigitalInOut(board.D15)]
+		         digitalio.DigitalInOut(board.D4),
+                         digitalio.DigitalInOut(board.D14)]
 
-AXON_BARREL_PIN = digitalio.DigitalInOut(board.D21)
+AXON_BARREL_PIN = digitalio.DigitalInOut(board.D15)
 
 # Initialize all pins
 
