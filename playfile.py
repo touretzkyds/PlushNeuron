dfc9664e1bf445f60d15727916c6a43f084d0c4e
Dave <dst@cs.cmu.edu>
2025-01-22 15:33:45 -0500

    New sounds

--- playfile.py
+++ playfile.py
@@ -1,5 +1,12 @@
+import argparse
+import time
 import pygame
 from pygame.mixer import Sound
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', type=str)
+
+args = parser.parse_args()
 
 pygame.mixer.init()
 pygame.init()
@@ -7,9 +14,13 @@ pygame.mixer.music.set_volume(0.9) #volume at 90%
 
 channel = pygame.mixer.Channel(0)
 
-filename = "sounds/dendrite3/weight-transitions/trombone_E4-G4.ogg"
+#filename = "sounds/dendrite3/weight-transitions/trombone_E4-G4.ogg"
+filename = args.filename
 
 print("Playing", filename)
 
 channel.play(Sound(filename))
+
+while channel.get_busy():
+    time.sleep(0.1)
 
